1047998e437e1aba5c9407db4a5db6c64ff7a84d
ScriptReader420 <fransie.nel06@gmail.com>
2025-05-27 12:48:37 +0200

    Models and base template added.

--- party/admin.py
+++ party/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser, Party, Gift, Guest
+
+
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+    
